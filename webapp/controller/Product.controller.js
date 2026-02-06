f77b31af9e3c61506f12e05ff7b15c49c9f96665
Amazonka20 <polinavinnikova0@gmail.com>
2026-02-06 13:45:50 +0200

    [Task 5-2] New page

---
+++ webapp/controller/Product.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define(["project2/controller/BaseController"], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("project2.controller.Product", {
+    onInit() {},
+  });
+});
