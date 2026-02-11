432b00e6b88205ec4993a5597cf198f64bd2a120
Amazonka20 <polinavinnikova0@gmail.com>
2026-02-11 14:53:18 +0200

    [Task 6-2] oData v4 – Record Creation

--- webapp/controller/ODataV4Tab.controller.js
+++ webapp/controller/ODataV4Tab.controller.js
@@ -4,8 +4,9 @@ sap.ui.define(
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageBox",
     "sap/m/MessageToast",
+    "project2/utils/DialogValidator",
   ],
-  (BaseController, JSONModel, MessageBox, MessageToast) => {
+  (BaseController, JSONModel, MessageBox, MessageToast, DialogValidator) => {
     "use strict";
 
     return BaseController.extend("project2.controller.ODataV4Tab", {
@@ -36,6 +37,97 @@ sap.ui.define(
         });
       },
 
+      async onOpenAddDialog() {
+        const oDialog = await this._getDialog();
+        oDialog.getModel("record").setData(this._getEmptyRecord());
+
+        oDialog.open();
+      },
+
+      onCloseDialog(oEvent) {
+        const oDialog = oEvent.getSource().getParent();
+        this._resetDialogValidation();
+        oDialog.close();
+      },
+
+      onSaveDialog(oEvent) {
+        if (!this._validateDialog()) {
+          return;
+        }
+        const oTable = this._getBooksTable();
+        const oListBinding = oTable.getBinding("items");
+        const oPayload = this.oDialog.getModel("record").getData();
+        const oNewContext = oListBinding.create(oPayload);
+
+        oNewContext
+          .created()
+          .then(() => {
+            MessageToast.show(this.getI18nText("msgCreateSuccess"));
+
+            this.onCloseDialog(oEvent);
+          })
+          .catch(() => {
+            MessageToast.show(this.getI18nText("msgCreateError"));
+            this.onCloseDialog(oEvent);
+          });
+      },
+
+      _validateDialog() {
+        const oData = this.oDialog.getModel("record").getData();
+
+        return DialogValidator.validateDialog({
+          data: oData,
+          getI18nText: this.getI18nText.bind(this),
+          fields: [
+            {
+              key: "Name",
+              control: this.byId("inpNameV4"),
+              required: true,
+            },
+            {
+              key: "Description",
+              control: this.byId("inpDescriptionV4"),
+              required: true,
+            },
+            {
+              key: "ReleaseDate",
+              control: this.byId("dpReleaseDateV4"),
+              required: true,
+              type: "date",
+            },
+            {
+              key: "DiscontinuedDate",
+              control: this.byId("dpDiscontinuedDateV4"),
+              required: false,
+              type: "date",
+            },
+            {
+              key: "Rating",
+              control: this.byId("inpRatingV4"),
+              required: true,
+            },
+            {
+              key: "Price",
+              control: this.byId("inpPriceV4"),
+              required: true,
+            },
+          ],
+        });
+      },
+
+      _resetDialogValidation() {
+        DialogValidator.resetDialogValidation({
+          controls: [
+            this.byId("inpNameV4"),
+            this.byId("inpDescriptionV4"),
+            this.byId("dpReleaseDateV4"),
+            this.byId("dpDiscontinuedDateV4"),
+            this.byId("inpRatingV4"),
+            this.byId("inpPriceV4"),
+          ],
+        });
+      },
+
       _doDelete() {
         const oTable = this._getBooksTable();
         const aContexts = oTable.getSelectedContexts(true);
@@ -56,6 +148,28 @@ sap.ui.define(
           });
       },
 
+      async _getDialog() {
+        if (!this.oDialog) {
+          this.oDialog = await this.loadFragment({
+            name: "project2.fragments.ODataV4Dialog",
+          });
+
+          const oNewModel = new JSONModel(this._getEmptyRecord());
+          this.oDialog.setModel(oNewModel, "record");
+        }
+        return this.oDialog;
+      },
+
+      _getEmptyRecord() {
+        return {
+          Name: "",
+          Description: "",
+          ReleaseDate: null,
+          DiscontinuedDate: null,
+          Rating: 0,
+          Price: 0,
+        };
+      },
       _resetSelection(oTable) {
         oTable.removeSelections(true);
         this.getModel("view").setProperty("/bCanDelete", false);
